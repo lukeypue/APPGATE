82f76ace5305b541863171ce8c2a8102b6e3ecff
lukeypue <lukeypue@gmail.com>
2026-09-13 03:40:53 -0600

    feat: add continuous site learning session

---
+++ app/src/main/java/com/appgate/tv/sitebrain/ContinuousLearningSession.kt
@@ -0,0 +1,23 @@
+package com.appgate.tv.sitebrain
+
+class ContinuousLearningSession private constructor(
+    val policy: AutonomousLearningPolicy,
+    var actionsTaken: Int = 0,
+        private set
+) {
+    fun recordAction() {
+        if (actionsTaken < policy.maxActions) actionsTaken++
+    }
+
+    fun shouldContinue(coverage: Double, elapsedMs: Long): Boolean =
+        policy.shouldContinue(
+            coverage = coverage,
+            actionsTaken = actionsTaken,
+            elapsedMs = elapsedMs
+        )
+
+    companion object {
+        fun forSource(sourceKey: String): ContinuousLearningSession =
+            ContinuousLearningSession(AutonomousLearningPolicy.forSource(sourceKey))
+    }
+}

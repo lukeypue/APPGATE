a7cdb2ac1f52da46de7b261c930cd5ad156f01c4
lukeypue <lukeypue@gmail.com>
2026-09-22 12:08:18 -0600

    learning: focus Site Brain on hard-to-search sources only

--- app/src/main/java/com/appgate/tv/sitebrain/LearningSiteCatalog.kt
+++ app/src/main/java/com/appgate/tv/sitebrain/LearningSiteCatalog.kt
@@ -14,37 +14,35 @@ data class LearningSite(
 }
 
 object LearningSiteCatalog {
+    /**
+     * Dedicated-brain training catalog.
+     *
+     * Keep this small: these are sites where generic web search is not enough
+     * for the deep-search behavior we need (dynamic/session UI, site-specific
+     * filters, or listing-detail checks). Public retailer/search-engine sites
+     * are intentionally NOT trained here; a general web/search layer can cover
+     * those later without spending Site Brain time relearning them.
+     *
+     * Facebook Marketplace is trained separately in FacebookGeckoPilotActivity.
+     */
     fun defaultSites(): List<LearningSite> = listOf(
-        LearningSite("facebook_marketplace", "Facebook Marketplace", "https://www.facebook.com/marketplace/", "facebook.com"),
-        LearningSite("ksl_classifieds", "KSL Classifieds", "https://classifieds.ksl.com/", "classifieds.ksl.com"),
-        LearningSite("ksl_cars", "KSL Cars", "https://cars.ksl.com/", "cars.ksl.com"),
-        LearningSite("ebay", "eBay", "https://www.ebay.com/", "ebay.com"),
-        LearningSite("craigslist", "Craigslist", "https://www.craigslist.org/", "craigslist.org"),
-        LearningSite("offerup", "OfferUp", "https://offerup.com/", "offerup.com"),
-        LearningSite("autotrader", "AutoTrader", "https://www.autotrader.com/", "autotrader.com"),
-        LearningSite("cars_com", "Cars.com", "https://www.cars.com/", "cars.com"),
-        LearningSite("carmax", "CarMax", "https://www.carmax.com/", "carmax.com"),
-        LearningSite("carfax", "CARFAX", "https://www.carfax.com/", "carfax.com"),
-        LearningSite("carvana", "Carvana", "https://www.carvana.com/", "carvana.com"),
-        LearningSite("kbb", "Kelley Blue Book", "https://www.kbb.com/", "kbb.com"),
-        LearningSite("carsforsale", "CarsForSale.com", "https://www.carsforsale.com/", "carsforsale.com"),
-        LearningSite("privateauto", "PrivateAuto", "https://privateauto.com/", "privateauto.com"),
-        LearningSite("truecar", "TrueCar", "https://www.truecar.com/", "truecar.com"),
-        LearningSite("cargurus", "CarGurus", "https://www.cargurus.com/", "cargurus.com"),
-        LearningSite("edmunds", "Edmunds", "https://www.edmunds.com/", "edmunds.com"),
-        LearningSite("autolist", "Autolist", "https://www.autolist.com/", "autolist.com"),
-        LearningSite("hemmings", "Hemmings", "https://www.hemmings.com/", "hemmings.com"),
-        LearningSite("cars_bids", "Cars & Bids", "https://carsandbids.com/", "carsandbids.com"),
-        LearningSite("bringatrailer", "Bring a Trailer", "https://bringatrailer.com/", "bringatrailer.com"),
-        LearningSite("bestbuy", "Best Buy", "https://www.bestbuy.com/", "bestbuy.com"),
-        LearningSite("walmart", "Walmart", "https://www.walmart.com/", "walmart.com"),
-        LearningSite("target", "Target", "https://www.target.com/", "target.com"),
-        LearningSite("amazon", "Amazon", "https://www.amazon.com/", "amazon.com"),
-        LearningSite("etsy", "Etsy", "https://www.etsy.com/", "etsy.com"),
-        LearningSite("homedepot", "Home Depot", "https://www.homedepot.com/", "homedepot.com"),
-        LearningSite("lowes", "Lowe's", "https://www.lowes.com/", "lowes.com"),
-        LearningSite("newegg", "Newegg", "https://www.newegg.com/", "newegg.com"),
-        LearningSite("google", "Google", "https://www.google.com/", "google.com"),
-        LearningSite("bing", "Bing", "https://www.bing.com/", "bing.com")
+        LearningSite(
+            "ksl_classifieds",
+            "KSL Classifieds",
+            "https://classifieds.ksl.com/",
+            "classifieds.ksl.com"
+        ),
+        LearningSite(
+            "ksl_cars",
+            "KSL Cars",
+            "https://cars.ksl.com/",
+            "cars.ksl.com"
+        ),
+        LearningSite(
+            "offerup",
+            "OfferUp",
+            "https://offerup.com/",
+            "offerup.com"
+        )
     )
 }
